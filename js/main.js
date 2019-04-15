f930b73062dd0b88a536b2e69b97fc07d222e50e
PetejStone <petejstoney@gmail.com>
2019-04-15 11:08:54 -0700

    basic layout and initial commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const navIcon = document.querySelector('#nav-icon');
+const nav = document.querySelector('.navigation');
+const heading = document.querySelector('h1');
+const about = document.querySelector('.about');
+const close = document.querySelector('.close');
+const aboutLink = document.querySelector('#about-link');
 
 navIcon.addEventListener('click',()=> {
   navIcon.classList.toggle("change");
-
+  nav.classList.toggle('show');
+  heading.classList.toggle('slide');
 });
+
+aboutLink.addEventListener('click', ()=> {
+  about.classList.toggle('slide-in');
+});
+
+close.addEventListener('click', ()=>{
+  about.classList.remove('slide-in');
+});
+
+//
