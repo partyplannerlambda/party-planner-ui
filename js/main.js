159e699b7671c7cd4119e266bc3c2626a5322fed
PetejStone <petejstoney@gmail.com>
2019-04-18 12:01:04 -0700

    fixed validation errors and added pulse animation

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ navIcon.addEventListener('click',()=> {
 class PageLink {
   constructor(link) {
     this.link = link;
-    console.log(this.link);
+    //console.log(this.link);
      this.link.addEventListener('click', this.slideIn.bind(this));
 
   }
